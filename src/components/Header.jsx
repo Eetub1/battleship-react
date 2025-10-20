88d0ef9478c448db0a1512ed252e8fc006ff5a83
eeeeoino <eetu.oinonen04@gmail.com>
2025-10-20 22:35:28 +0300

    added an info dialog that tells info about the game

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,45 @@
-import { Button } from 'react-bootstrap'
+import { Modal, Button } from 'react-bootstrap'
+import { useState } from 'react'
 
 const Header = () => {
-    const buttonStyle = {border: 'none', marginLeft: '5px', borderRadius: '7px', width: '70px', height: '30px', color: 'white', backgroundColor: 'grey'}
+    const [show, setShow] = useState(false)
+    const handleShow = () => {setShow(true)}
+    const handleClose = () => {setShow(false)}
 
     return (
-        <div style={{minHeight: '50px'}} className=' d-flex align-items-center text-white bg-dark'>
-            <button style={buttonStyle}>Info</button>
+        <div style={{minHeight: '70px'}} className='d-flex align-items-center text-white bg-dark'>
+            <Button className='ms-2' variant='secondary' onClick={handleShow}>Info</Button>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                centered
+                animation={true}
+                dialogClassName="info-modal">
+
+                <Modal.Header closeButton>
+                    <Modal.Title>Game Rules</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <h5>How to Play</h5>
+                    <ul>
+                        <li>Place your ships on the board</li>
+                        <li>Try to predict where the enemy ships are located</li>
+                        <li>First to eliminate their enemy's ships wins!</li>
+                    </ul>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+
+            </Modal>
         </div>
+
+        
     )
 } 
 
